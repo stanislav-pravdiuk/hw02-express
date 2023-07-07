4921c13f63796ee9c8a28984744c45afe10dc909
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-07-07 16:03:32 +0300

    add
    token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,5 +5,6 @@ const { ValidateBody } = require('../../middleware')
 const { schemas } = require('../../models/user')
 
 router.post("/register", ValidateBody(schemas.registerSchema), ctrl.register);
+router.post("/login", ValidateBody(schemas.loginSchema), ctrl.login);
 
 module.exports = router;
