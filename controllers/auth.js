a46abc99dc20e7361993ec15f4db2d4e267f607b
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-07-12 12:47:36 +0300

    add
    authenticate

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,7 +11,7 @@ const register = async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (user) {
-        throw HttpError(409, 'Email allready in use')
+        throw HttpError(409, 'Email in use')
     }
     const hashPassword = await bcrypt.hash(password, 10);
     const newUser = await User.create({...req.body, password: hashPassword});
@@ -25,18 +25,20 @@ const login = async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (!user) { 
-        throw HttpError(401, 'Email or password invalid')
+        throw HttpError(401, 'Email or password is wrong')
     };
     const passwordCompare = await bcrypt.compare(password, user.password);
     if (!passwordCompare) {
-        throw HttpError(401, 'Email or password invalid'); 
+        throw HttpError(401, 'Email or password is wrong'); 
     };
     const payload = {
         id: user._id
     };
-    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: '23h'});
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '23h' });
     res.json({
         token,
+        email,
+        user: user.subscription
     });
 };
 
--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,9 @@
 const ValidateBody = require('./ValidateBody');
 const isValidId = require('./isValidId');
-
+const authenticate = require('./authenticate');
 
 module.exports = {
     ValidateBody,
     isValidId,
+    authenticate,
 };
