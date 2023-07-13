2ae9a579049bc0102a9291eb8ca2bfff2b9828d7
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-07-13 15:35:26 +0300

    done

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const ctrl = require('../../controllers/auth');
 const router = express.Router();
-const { ValidateBody, authenticate } = require('../../middleware')
+const { ValidateBody, authenticate, upload } = require('../../middleware')
 const { schemas } = require('../../models/user')
 
 router.post("/register", ValidateBody(schemas.registerSchema), ctrl.register);
 router.post("/login", ValidateBody(schemas.loginSchema), ctrl.login);
 router.get('/current', authenticate, ctrl.getCurrent);
 router.post('/logout', authenticate, ctrl.logout);
+router.patch('/avatars', authenticate, upload.single("avatar"), ctrl.updateAvatar )
 
 module.exports = router;
